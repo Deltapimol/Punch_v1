56b39b32439c9cadd0e164264151290cb05a7a28
Deltapimol <anmolshinde102@gmail.com>
2020-11-23 00:36:51 +0530

    Added buzzer and timer reset sounds. Extra time addition and other conditions added.

--- lib/punch.dart
+++ lib/punch.dart
@@ -3,6 +3,8 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
+import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
 import './login.dart';
 import './config.dart';
 
@@ -22,11 +24,18 @@ class _PunchState extends State<Punch> {
   int constTimer = 0;
   int _timer = 0;
   bool timerActive = false;
+  bool time_interval = false;
   Timer _timerClass;
+  bool punched = false;
+  int extraTime = 0;
+  AudioCache _audioCache;
 
   @override
   void initState() {
     super.initState();
+    _audioCache = AudioCache(
+        prefix: "audio/",
+        fixedPlayer: AudioPlayer()..setReleaseMode(ReleaseMode.STOP));
     checkLoginStatus();
     SharedPreferences.getInstance().then((SharedPreferences sp) {
       sharedPreferences = sp;
@@ -55,7 +64,20 @@ class _PunchState extends State<Punch> {
       oneSec,
       (Timer timer) => setState(
         () {
-          _timer = _timer - 1;
+          if (_timer == 0) {
+            if (punched == false) {
+              extraTime += constTimer;
+              print("extra time");
+              print(extraTime);
+            }
+            _timer = constTimer;
+            _audioCache.play('buzzer.mp3');
+            print("TIMEE");
+            print(timerActive);
+            timerActive = false;
+          } else {
+            _timer = _timer - 1;
+          }
         },
       ),
     );
@@ -64,18 +86,15 @@ class _PunchState extends State<Punch> {
   int Punch() {
     setState(() {
       print(_punchCount);
-      if ((constTimer - _timer) > 5 || timerActive == false) {
+      if (timerActive == false) {
+        print("TIMEE2");
+        print(timerActive);
         punchRequest();
         _punchCount += 1;
-
-        if (timerActive == false) {
-          startTimer();
-          timerActive = true;
-        } else {
-          _timer = constTimer;
-        }
         print("PUNCH!");
-        print(_punchCount);
+        timerActive = true;
+        startTimer();
+        _audioCache.play('timer_reset.mp3');
       }
     });
 
@@ -93,18 +112,21 @@ class _PunchState extends State<Punch> {
 
   Future<void> punchRequest() async {
     ConfigClass config = ConfigClass();
-    print("URL " + config.getBaseUrl() + "/api/v1/controller/punch");
-    print("IN PUNCH! " +
-        sharedPreferences.getString("token") +
-        " " +
-        sharedPreferences.getString("unique_id"));
+    int actualTime = 0;
+    if (extraTime != 0) {
+      actualTime = extraTime + _timer;
+      print("ACTUAL TIME");
+      print(actualTime);
+      setState(() {
+        extraTime = 0;
+      });
+    }
     Map data = {
       "unique_id": sharedPreferences.getString("unique_id"),
-      "time_required":
-          (_timer < 1) ? (constTimer + (-_timer)) : (constTimer - _timer)
+      "time_required": (actualTime != 0) ? (actualTime) : (constTimer - _timer)
     };
     String body = json.encode(data);
-    print(body);
+    // print(body);
     try {
       final response = await http.post(
         config.getBaseUrl() + "/api/v1/controller/punch",
@@ -114,10 +136,11 @@ class _PunchState extends State<Punch> {
         },
         body: body,
       );
-      print("STATUS CODE");
-      print(response.statusCode);
       if (response.statusCode == 201) {
         print("PUNCH DONE?");
+        setState(() {
+          punched = true;
+        });
       } else {
         throw Exception('Failed to punch');
       }
@@ -129,11 +152,6 @@ class _PunchState extends State<Punch> {
 
   Future<void> logout() async {
     ConfigClass config = ConfigClass();
-    print("URL " + config.getBaseUrl() + "/api/v1/account/login");
-    print("IN LOGOUT! " +
-        sharedPreferences.getString("token") +
-        " " +
-        sharedPreferences.getString("unique_id"));
     Map data = {"unique_id": sharedPreferences.getString("unique_id")};
     String body = json.encode(data);
     try {
@@ -145,8 +163,6 @@ class _PunchState extends State<Punch> {
         },
         body: body,
       );
-      print("STATUS CODE");
-      print(response.statusCode);
       if (response.statusCode == 200) {
         print("LOGOUT DONE?");
       } else {
@@ -156,13 +172,6 @@ class _PunchState extends State<Punch> {
       print("Exception $a");
       print("Stacktrace $e");
     }
-    // print("STATUS CODE");
-    // print(response.statusCode);
-    // if (response.statusCode == 200) {
-    //   print("LOGOUT DONE?");
-    // } else {
-    //   throw Exception('Failed to logout');
-    // }
   }
 
   @override
@@ -220,7 +229,7 @@ class _PunchState extends State<Punch> {
                               ] // make rounded corner of border
                               ),
                           child: Text(
-                            'Time : $_timer' + ' ' + 'seconds\n',
+                            'Timer: $constTimer seconds\n \n Time : $_timer seconds\n',
                             style: TextStyle(fontSize: 25, color: Colors.white),
                           ),
                         ),
